0f6d33936c2f188c804418bc22a31b6acf4f0fb8
Aditya Singh <68491442+adityasingh2509@users.noreply.github.com>
2021-02-14 21:48:47 +0530

    Updated

---
+++ c-practice/Sum-and-Difference-of-Two-Numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int num1, num2;
+    float num3, num4;
+    
+    // Scan the Numbers
+    scanf("%d %d", &num1, &num2);
+    scanf("%f %f", &num3, &num4);
+    
+    //Print to Console
+    printf("%d %d\n", num1 + num2, num1 - num2);
+    printf("%0.1f %0.1f", num3 + num4, num3 - num4);
+    
+    return 0;
+}
